df8c7fbe9e9f2352c60aec84cfa173ccd9508da8
Jung Yeonwoo <gsh95214@gmail.com>
2025-06-13 17:42:57 +0900

    스레드 풀과 Executor 프레임워크1

--- src/thread/executor/future/SumTaskMainV1.java
+++ src/thread/executor/future/SumTaskMainV1.java
@@ -1,35 +1,31 @@
-package thread.control.join;
+package thread.executor.future;
 
 import static util.MyLogger.log;
-import static util.ThreadUtils.sleep;
 
-public class JoinMainV1 {
-
-    public static void main(String[] args) {
-        log("Start");
+public class SumTaskMainV1 {
+    public static void main(String[] args) throws InterruptedException {
         SumTask task1 = new SumTask(1, 50);
         SumTask task2 = new SumTask(51, 100);
         Thread thread1 = new Thread(task1, "thread-1");
         Thread thread2 = new Thread(task2, "thread-2");
-
         thread1.start();
         thread2.start();
-
-        log("task1.result = " + task1.result);
-        log("task2.result = " + task2.result);
-
+        //스레드가 종료될 때 까지 대기
+        log("join() - main 스레드가 thread1, thread2 종료까지 대기");
+        thread1.join();
+        thread2.join();
+        log("main 스레드 대기 완료");
+        log("task1.result=" + task1.result);
+        log("task2.result=" + task2.result);
         int sumAll = task1.result + task2.result;
-
         log("task1 + task2 = " + sumAll);
         log("End");
-
     }
 
     static class SumTask implements Runnable {
-
         int startValue;
         int endValue;
-        int result;
+        int result = 0;
 
         public SumTask(int startValue, int endValue) {
             this.startValue = startValue;
@@ -39,13 +35,17 @@ public class JoinMainV1 {
         @Override
         public void run() {
             log("작업 시작");
-            sleep(2000);
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
             int sum = 0;
-            for(int i = startValue ; i <= endValue; i++){
+            for (int i = startValue; i <= endValue; i++) {
                 sum += i;
             }
             result = sum;
-            log("작업 완료 result = " + result);
+            log("작업 완료 result=" + result);
         }
     }
 }
